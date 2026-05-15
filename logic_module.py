4f73222e837bd05599010943625f92620456bd16
bukolamiracle531-jpg <bukolamiracle531@gmail.com>
2026-05-15 07:50:19 +0100

    Create logic_module.py

---
+++ logic_module.py
@@ -0,0 +1,8 @@
+def calculate_grade(score):
+    if score is None: return "Invalid"
+    if score >= 70: return 'A'
+    elif score >= 60: return 'B'
+    elif score >= 50: return 'C'
+    elif score >= 45: return 'D'
+    elif score >= 40: return 'E'
+    else: return 'F'
